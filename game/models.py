012c15f2eea6590b2cfeaf53297fd4477b8a2658
Clemens Macho <email@noreply.com>
2021-06-17 20:08:04 +0200

    implement models and login/logout
    User, Game and Move models.
    created various templates login.html, layout.html, index.html, register.html
    styles.css is empty for now.

--- game/models.py
+++ game/models.py
@@ -1,7 +1,38 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import datetime
 
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    rating = models.IntegerField(default = 1000)
+
+class Game(models.Model):
+    COLORS = [('R', 'Red'), ('B','Blue'), ('N', 'No color')]
+    player1 = models.ForeignKey("User", on_delete=models.PROTECT, related_name='games_p1')
+    player2 = models.ForeignKey("User", on_delete=models.PROTECT, related_name='games_p2')
+    player1color = models.CharField(max_length=2, choices=COLORS, default='N')
+    latest_move_num = models.IntegerField()
+    latest_move_datetime = models.DateTimeField()
+    board = models.JSONField()
+    time_used_p1 = models.DurationField(default = datetime.timedelta()) #default is 0
+    time_used_p2 = models.DurationField(default = datetime.timedelta()) #default is 0
+    created_at = models.DateTimeField(auto_now_add=True)
+    game_started_at = models.DateTimeField()
+    time_per_player = models.DurationField()
+    time_increment_per_move = models.DurationField()
+
+    def __str__(self):
+        return f"game {self.id} created by {self.player1} at {self.created_at}"
+
+class Move(models.Model):
+    COLORS = [('R', 'Red'), ('B','Blue')]
+    game = models.ForeignKey('Game', on_delete=models.CASCADE, related_name='moves')
+    x = models.IntegerField()
+    y = models.IntegerField()
+    color = models.CharField(max_length=1, choices=COLORS)
+    player = models.IntegerField() # player 1 or 2
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.color} at ({self.x},{self.y}) at {self.timestamp}"
--- game/views.py
+++ game/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from .models import User
 
 # Create your views here.
+def index(request):
+    return render(request, 'game/index.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, 'game/login.html', {
+                'message': 'Invalid username and/or password.'
+            })
+    else:
+        return render(request, 'game/login.html')
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+
+        # ensure password matches confirmation
+        password = request.POST['password']
+        confirmation = request.POST['confirmation']
+        if password != confirmation:
+            return render(request, 'game/register.html', {
+                'message': 'Passwords must match.'
+            })
+
+        # attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, 'game/register.html', {
+                'message': 'Username already taken.'
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse('index'))
+    else:
+        return render(request, 'game/register.html')
